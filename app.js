f624c312d0a2c12ac8b82a5f65bead1a25d26cb3
unknown <kalinowskibartosz@gmail.com>
2023-09-22 14:36:46 +0200

    add cron jobs

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const dataRoutes = require("./routes/dataRoutes");
 const views = require("./views/views.js");
 
-const { insterForecastToDb } = require("./utils/insterFetchToDb");
+const cron = require("./utils/cron");
 
 const app = express();
 const port = process.env.PORT;
@@ -16,7 +16,7 @@ app.use(
 app.use("/", views);
 app.use("/api", dataRoutes);
 
-insterForecastToDb();
+cron();
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
