0ccd0818622a2e286f1d685d87bd6afdbecb25bd
unknown <kalinowskibartosz@gmail.com>
2023-09-27 12:38:45 +0200

    token

--- routes/tokenShow.js
+++ routes/tokenShow.js
@@ -6,72 +6,13 @@ const tokenShow = express.Router();
 
 tokenShow.get("/1", async (req, res) => {
   try {
+    console.log(__dirname);
+    console.log(path.dirname(__filename));
     const data = await fs.readFileSync("/token.json");
     res.send(data);
   } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/2", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/3", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("current/token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/4", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("/current/token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/5", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("/app/current/token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/6", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("app/current/token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/7", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("../token.json");
-    res.send(data);
-  } catch (error) {
-    console.error("Error creating show token URL:", error);
-    res.status(500).send("Error showing token");
-  }
-});
-tokenShow.get("/8", async (req, res) => {
-  try {
-    const data = await fs.readFileSync("../../token.json");
-    res.send(data);
-  } catch (error) {
+    console.log(__dirname);
+    console.log(path.dirname(__filename));
     console.error("Error creating show token URL:", error);
     res.status(500).send("Error showing token");
   }
