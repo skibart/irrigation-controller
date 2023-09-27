437a669c67aa3ddf8cae600c94ebdb8848601cb4
unknown <kalinowskibartosz@gmail.com>
2023-09-27 13:08:25 +0200

    token

--- routes/tokenRoute.js
+++ routes/tokenRoute.js
@@ -40,7 +40,7 @@ router.get("/redirectUrl", async (req, res) => {
     });
 
     response.region = region;
-    const filename = "token.json";
+    const filename = "test.json";
     const filePath = path.resolve(process.cwd(), filename);
     fs.writeFileSync(filePath, JSON.stringify(response));
     const responseData = {
